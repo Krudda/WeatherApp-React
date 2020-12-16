019f68b75c46099755918898e7a4a5e0160fbd52
Dmitriy <krudda@yandex.ru>
2020-12-16 21:51:11 +0300

    feat(#1, #2): create app structure, add api keys

---
+++ weather-react-app/src/data/apiKeys.js
@@ -0,0 +1,8 @@
+export const keys = {
+    imageKEY: 'HPrX1b-bsu7MQMIhigql13XsvOq5DnSXZ4KcKfJ-qdg',
+    weatherKEY: 'd20a03f0a99786d8a9e4cd59735d63ae',
+    weather2KEY: 'bacc209cb9414b53a04155157203105',
+    weather3KEY: 'b0e7e615386346d597cac9c3d7905c61',
+    ipKEY: '1ba1b626152ab3',
+    mapKEY: '0dc6eb55-446b-488d-9432-18119fb70dff',
+  };
