d9c3c300d5710fc08d3d3d3b7f70afd5cb709b2f
Dmitriy <krudda@yandex.ru>
2021-01-29 14:17:11 +0300

    feat(#42): weather icons according to local time

--- weather-react-app/src/components/common/WeatherImage/index.js
+++ weather-react-app/src/components/common/WeatherImage/index.js
@@ -1,11 +1,25 @@
+import {useSelector} from 'react-redux';
 import styles from './weatherImage.module.scss'
 
 const WeatherImage = ({image, addClass})  => {
     const {id_icon, description} = image;
     const extendClass = addClass? `${addClass}_` : '';
 
-    const imgURL_svg = `/images/${id_icon}.svg`;
-    const imgURL_png = `/images/${id_icon}.png`;
+    const timeZone = useSelector(state => state.weather.weather.timezone);
+    const currentTimeZone = timeZone ? timeZone : 'Europe/Moscow';
+    const now = new Date().toLocaleTimeString('en-GB', { timeZone: currentTimeZone });
+    const nowHours =  now.split(':')[0];
+    const times = (nowHours > 19 || nowHours < 9) ? 'n' : 'd';
+    let correctIconID = id_icon;
+
+    if (times === 'n') {
+        const id_iconArr = id_icon.split('');
+        id_iconArr[3] = 'n';
+        correctIconID = id_iconArr.join('');
+    }
+
+    const imgURL_svg = `/images/${correctIconID}.svg`;
+    const imgURL_png = `/images/${correctIconID}.png`;
 
     const handleImageError = (e) => {
         e.target.src = imgURL_png;
