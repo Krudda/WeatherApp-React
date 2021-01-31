d8f1f21d89ade147de48d30364b9613ad2a16288
Dmitriy <krudda@yandex.ru>
2021-01-31 18:31:51 +0300

    feat: add second ip-handler

--- weather-react-app/src/services/getUserLocation.js
+++ weather-react-app/src/services/getUserLocation.js
@@ -1,16 +1,21 @@
 import { keys } from '../data/apiKeys';
 
-const key = keys.ip2KEY;
+const key = keys.ipKEY;
+const key2 = keys.ip2KEY;
 
 async function getUserLocation() {
-  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${key}`;
+  const url1 = `https://api.ipgeolocation.io/ipgeo?apiKey=${key2}`;
+  const url2 = `https://ipinfo.io/json?token=${key}`;
   try {
-    const res = await fetch(url);
+    const res = await fetch(url1);
     const data = await res.json();
     const location = {city: data.city.toLowerCase(), country: data.country_name.toLowerCase()};
-    console.log('location', location)
     return location;
   } catch (err) {
+    const res = await fetch(url2);
+    const data = await res.json();
+    const location2 = {city: data.city.toLowerCase(), country: data.country.toLowerCase()};
+    return location2;
     // return {city: 'Nizhniy Novgorod', country: 'RU'}
   }
 }
